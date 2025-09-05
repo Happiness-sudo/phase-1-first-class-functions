5228a7217cb1dc8dd16688697f98756f7044d83d
Happiness Ngeete <ndolohappiness@gmail.com>
2025-09-05 08:48:57 +0300

    Implement first-class functions lab: receivesAFunction, returnsANamedFunction, returnsAnAnonymousFunction

---
+++ index.js
@@ -0,0 +1,22 @@
+function receivesAFunction(callback) {
+  callback();
+}
+
+function returnsANamedFunction() {
+  function namedFunction() {
+    return "I am a named function!";
+  }
+  return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    return "I am an anonymous function!";
+  };
+}
+
+module.exports = {
+  receivesAFunction,
+  returnsANamedFunction,
+  returnsAnAnonymousFunction,
+};
